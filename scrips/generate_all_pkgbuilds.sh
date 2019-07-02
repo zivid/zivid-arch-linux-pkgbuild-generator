521e86fc738b5311561372a3ffea5d45eb1740e0
Mathias Nedrebø <mathias.nedrebo@zividlabs.com>
2019-07-02 16:06:14 +0200

    Better error handling in shell scripts

--- scrips/generate_all_pkgbuilds.sh
+++ scrips/generate_all_pkgbuilds.sh
@@ -2,7 +2,7 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ROOT_DIR=$(realpath "$SCRIPT_DIR/..")
-VENV=$(mktemp --tmpdir --directory zivid-pkgbuild-build-env-XXXX)
+VENV=$(mktemp --tmpdir --directory zivid-pkgbuild-build-env-XXXX) || exit $?
 
 if [ -z "$1" ]; then
     echo Usage: $0 out-dir
